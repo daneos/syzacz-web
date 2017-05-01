c9299720d2f7f96311fa48348ac7d8b40f2e464e
daneos <daneos@daneos.com>
2017-05-01 15:50:43 +0200

    little changes

--- core_sv/urls.py
+++ core_sv/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
 
 	url(r'%s/test-core$' % base_url, 					'core.views.test_core'),
 
-	url(r'%s/login' % base_url,						'core.views.login'),
+	url(r'%s/login' % base_url,							'core.views.login'),
 	url(r'%s/logout$' % base_url,						'core.views.logout'),
 	url(r'%s/home$' % base_url,							'core.views.home')
 ]
