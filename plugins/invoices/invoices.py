2a642a88cc4f4470c42c94e0422a1d6239a94d77
Grzegorz Alwaro Pająk <gregory353a@gmail.com>
2017-05-30 12:40:59 +0200

    additional functions

--- plugins/invoices/invoices.py
+++ plugins/invoices/invoices.py
@@ -9,10 +9,13 @@ def init(plugin_env):
 
 def urls():
 	return [
-        ["%s/new_invoice/", "add_info_about_new_invoice", None], #templatka do wypelnienia informacji o nowej fakturze
+        ["%s/new_invoice/", "add_info", None], #templatka do wypelnienia informacji o nowej fakturze
         ["%s/add_invoice_file/", "add_file", None], #templatka do wyslania pliku faktury
         ["%s/show_invoices/", "check_invoices", None], #templatka do wyswietlenia wszystkich faktur i ich przegladania
         ["%s/invoices/", "invoices", None], #templatka do faktur transparency
         ["%s/show_invoice/", "show_invoice", None], #templatka do pokazania pojedynczej faktury
         ["%s/download_invoices", "download_all", None] #templatka do wszystkich faktur
 	]
+
+def add_info(rq):
+    return 0
