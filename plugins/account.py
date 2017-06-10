69b0fdbbc80c71a37dbc7f8d7b24785cf5f53d50
daneos <daneos@daneos.com>
2017-06-10 16:50:22 +0200

    integrated account

--- plugins/account.py
+++ plugins/account.py
@@ -1,5 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import Error
 from django.shortcuts import redirect
 
 from conf import app_base
@@ -11,7 +9,7 @@ env = {}
 def init(plugin_env):
     global env
     env = plugin_env
-    return Version([0, 0, 2, "alpha"])
+    return Version([0, 1, 0, "alpha"])
 
 
 def urls():
@@ -19,32 +17,20 @@ def urls():
         ["%s/account.status$", "account_status", "account/account_status.template.html"],
         ["%s/account.delete_session/(?P<session_id>[0-9]+)/$", "delete_session", None],
         ["%s/account.change_email$", "change_email", None],
-        ["%s/account.change_password", "change_password", None],
-        ["%s/account.settings$", "account_settings", "account/settings.template.html"]
+        ["%s/account.change_password$", "change_password", None],
+        ["%s/account.settings", "account_settings", "account/settings.template.html"]
     ]
 
 
 def account_status(rq):
     Session = env["getModel"]("Session")
     try:
-        session = Session.objects.get(session_hash=env["sessid"](rq))
-        sessions = Session.objects.filter(user=session.user, active=True)
-        user = session.user
-    except ObjectDoesNotExist:
-        return {"error": "Object does not exist"}
-    except Error:
-        return {"error": "error"}
+        current_session = Session.objects.get(session_hash=env["sessid"](rq))
+        sessions = Session.objects.filter(user=current_session.user, active=True)
+    except Exception as e:
+            return redirect("/%s/account.settings?error=%s" % (app_base, str(e)))
 
-    account_status = {}
-    account_status["name"] = user.name
-    account_status["login"] = user.cn
-    account_status["surname"] = user.surname
-    account_status["validity"] = user.validity
-    account_status["email"] = user.email
-    account_status["active_sessions"] = sessions
-    account_status["current_session"] = session
-
-    return account_status
+    return {"active_sessions": sessions, "current_session": current_session}
 
 
 def delete_session(rq, session_id):
@@ -54,20 +40,17 @@ def delete_session(rq, session_id):
         session = Session.objects.get(id=session_id)
         session.active = False
         session.save()
-    except ObjectDoesNotExist:
-        return {"error": "Object does not exist"}
-    except Error:
-        return {"error": "error"}
+    except Exception as e:
+            return redirect("/%s/account.settings?error=%s" % (app_base, str(e)))
 
-    return redirect("/%s/account.status" % app_base)
+    return redirect("/%s/account.status?msg=Session closed" % app_base)
 
 
 def account_settings(rq):
-    context = {}
+    context = {"msg": rq.GET.get("msg"), "error": rq.GET.get("error")}
 
     if rq.method == "GET":
         context.update(env["csrf"](rq))
-
     return context
 
 
@@ -77,15 +60,13 @@ def change_email(rq):
         try:
             session = Session.objects.get(session_hash=env["sessid"](rq))
             user = session.user
-        except ObjectDoesNotExist:
-            return {"error": "Object does not exist"}
-        except Error:
-            return {"error": "error"}
+        except Exception as e:
+            return redirect("/%s/account.settings?error=%s" % (app_base, str(e)))
 
         user.email = rq.POST.get("email")
         user.save()
-
-    return redirect("/%s/account.settings" % app_base)
+        return redirect("/%s/account.settings?msg=Email changed" % app_base)
+    return redirect("/%s/account.settings?error=Invalid request" % app_base)
 
 
 def change_password(rq):
@@ -94,10 +75,8 @@ def change_password(rq):
         try:
             session = Session.objects.get(session_hash=env["sessid"](rq))
             user = session.user
-        except ObjectDoesNotExist:
-            return {"error": "Object does not exist"}
-        except Error:
-            return {"error": "error"}
+        except Exception as e:
+            return redirect("/%s/account.settings?error=%s" % (app_base, str(e)))
 
         old_password = rq.POST.get("old_password")
 
@@ -105,6 +84,6 @@ def change_password(rq):
             user.password = rq.POST.get("new_password")
             user.save()
         else:
-            return {"error": "wrong password"}
+            return redirect("/%s/account.settings?error=Invalid password" % app_base)
 
-    return redirect("/%s/account.settings" % app_base)
+    return redirect("/%s/account.settings?msg=Password updated" % app_base)
