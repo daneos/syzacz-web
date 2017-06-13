c9b065bade520e223b2f9273309a22842cd80c26
daneos <daneos@daneos.com>
2017-06-13 13:01:13 +0200

    core version bump

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,6 +1,6 @@
 from core.version import Version
 
-version = Version([0, 0, 5, "testing"])
+version = Version([0, 0, 6, "testing"])
 app_base = "syzacz/%s" % version.url()
 verbose = True
 plugin_blacklist = ["testplugin", "secondtestplugin"]
